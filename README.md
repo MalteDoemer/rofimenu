657bcb7395ba61e2a1dd5ec05aa974b9850dd65e
Malte Dömer <maltedoemer@gmail.com>
2022-10-04 16:30:10 +0200

    updated README and setup.py

--- README.md
+++ README.md
@@ -2,8 +2,6 @@
 
 This is a small python project that uses [rofi](https://github.com/davatorium/rofi) to display menus.
 
-The `rofimenu.py` file contains a helper class named `RofiMenu`. All other files define custom menus.
-
 ## License
 
 Licensed under the MIT license ([LICENSE](LICENSE) or http://opensource.org/licenses/MIT)
--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 setup(
     name='rofimenu',
     version='0.1.0',
-    description='A example Python package',
-    url='',
+    description='A small python library that uses rofi to display menus.',
+    url='https://github.com/MalteDoemer/rofimenu',
     author='Malte Dömer',
     author_email='maltedoemer@gmail.com',
     license='MIT',
